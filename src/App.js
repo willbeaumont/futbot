57b04742e3c84c068fb753e26a2441055be26caf
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-12-05 23:50:49 -0800

    add TODO comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import './App.css';
+import "./App.css";
+import { Button, DialogBox, UserInput } from "./Compnents.js";
+
+//TODO: send request to api
+function getfDialogflow() {
+  alert("You need to update this!")
+}
 
 // stripped create-react-app to bare bones
 function App() {
@@ -8,6 +14,9 @@ function App() {
         <h1>Futbot</h1>
         <p>A soccer (or football) bot for the World Cup.</p>
       </header>
+      <DialogBox />
+      <UserInput />
+      <Button name="Send" action={getfDialogflow} />
     </div>
   );
 }
