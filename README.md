c6e2e35c607261460c8e57bee7243a14387bb499
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-12-05 23:22:59 -0800

    drop create-react-app boilerplate, add short description

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Getting Started with Create React App
+# Futbot
+
+
+---
+## React Boilerplate
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
+// stripped create-react-app to bare bones
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Futbot</h1>
+        <p>A soccer (or football) bot for the World Cup.</p>
       </header>
     </div>
   );
