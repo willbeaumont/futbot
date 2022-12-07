1ccdeefca3e67ba6c14422904e89ea324dabe2bd
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-12-06 20:28:23 -0800

    correct typo in file name, move api output description from component to app -- should avoid building a component for a specific api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import "./App.css";
-import { Button, DialogBox, UserInput } from "./Compnents.js";
+import { Button, DialogBox, UserInput } from "./Components.js";
+
+/* Here is a sample of what is returned by the Dialogflow API
+{
+  "id": "bc9c9f2f-b707-4bdd-aa1e-b5f053a8f8b3",
+  "timestamp": "2022-12-06T09:45:19.128Z",
+  "lang": "en",
+  "result": {
+    "source": "agent",
+    "resolvedQuery": "What is the weather like?",
+    "action": "",
+    "actionIncomplete": false,
+    "parameters": {},
+    "contexts": [],
+    "metadata": {
+      "intentId": "b5691684-a3e0-4d3e-befc-5f5b5d5b5d5d",
+      "webhookUsed": "false",
+      "webhookForSlotFillingUsed": "false",
+      "intentName": "Get Weather"
+    },
+    "fulfillment": {
+      "speech": "The weather is currently sunny and warm.",
+      "messages": [
+        {
+          "type": 0,
+          "speech": "The weather is currently sunny and warm."
+        }
+      ]
+    },
+    "score": 1
+  },
+  "status": {
+    "code": 200,
+    "errorType": "success"
+  },
+  "sessionId": "a1b2c3d4-e5f6-g7h8-i9j0"
+}
+*/
 
 //TODO: send request to api
 function getfDialogflow() {
